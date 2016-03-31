db4541490c4fe098560646552eb968598afb9a1a
hardinm78 <michaelhardin@Michaels-iMac.local>
2016-03-31 11:02:04 -0500

    temp retrieval functional

---
+++ Bad_Weather/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Bad_Weather
+//
+//  Created by Michael Hardin on 3/31/16.
+//  Copyright © 2016 Michael Hardin. All rights reserved.
+//
+
+import Foundation
+
+let URL_WEATHER = "http://api.openweathermap.org/data/2.5/forecast/city?id=4431410&APPID=7b57c8e052e224d49dfcbaaf9341f111"
+
+
+public typealias DownloadComplete = () -> ()
--- Bad_Weather/ViewController.swift
+++ Bad_Weather/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempLbl: UILabel!
+    var temperture : Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        downloadWeatherDetails { 
+            self.tempLbl.text = "\(self.temperture!)°"
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func downloadWeatherDetails(completed: DownloadComplete) {
+        
+        Alamofire.request(.GET, URL_WEATHER).responseJSON { response in
+            let result = response.result
+            
+            
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                
+                if let list = dict["list"] as? [Dictionary<String, AnyObject>] {
+                    
+                    if let main = list[0] ["main"] as? Dictionary<String,AnyObject> {
+                        print(main)
+                        
+                        let tempK = main["temp"] as? Int
+                        
+                        let tempF = Double(tempK!) * (9/5) - 459.67
+                        
+                        
+                        self.temperture = Int(tempF)
+                        
+                        completed()
+                    }
+                    
+                    
+                    
+                    
+                }
+                
+            }
+        }
+        
+        
+    }
+    
 
 }
 
